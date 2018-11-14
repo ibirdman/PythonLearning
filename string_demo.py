a14f3551fbd8b0a52a83d7126ea4016d092bcda6
Feng Gao <stefan.gao@sony.com>
2018-11-14 14:17:31 +0800

    I am starting to learn python from today.

---
+++ string_demo.py
@@ -0,0 +1,13 @@
+name = "ADa lOvelace"
+print(name.title())
+print(name.upper())
+print(name.lower())
+
+message = " Hello Python  ";
+print(message.strip())
+print(message.rstrip())
+print(message.lstrip())
+
+age = 10;
+wish = "Happy " + str(10) + "th birthday!"
+print(wish.strip())
